1609603cd08ae74bc90b1c49090de75f93da49b9
TOPAPEC <danbugrienko@yandex.ru>
2025-08-24 12:38:04 +0000

    tried to make wan 2.2 turbo work - no luck for now :(

--- run_infer_i2v_gguf.sh
+++ run_infer_i2v_gguf.sh
@@ -2,11 +2,11 @@
 set -e
 git config --global user.email "danbugrienko@yandex.ru"
 git config --global user.name "TOPAPEC"
-# sudo apt update
-# sudo apt install software-properties-common
-# sudo add-apt-repository ppa:deadsnakes/ppa
-# sudo apt update
-# sudo apt install python3.11 python3.11-venv python3.11-dev
+sudo apt update -y
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install -y python3.10 python3.10-venv python3.10-dev
 if [ ! -d .venv ]; then
   python -m venv .venv
 fi
@@ -21,8 +21,19 @@ PROJECT_DIR="$PWD"
 PARENT_DIR="$(dirname "$PROJECT_DIR")"
 SD_SCRIPTS_DIR="$PARENT_DIR/sd-scripts"
 
+cd ..
+git clone https://github.com/quanhaol/Wan2.2-TI2V-5B-Turbo.git
+cd Wan2.2-TI2V-5B-Turbo/
+python3.10 -m venv .venv
+. .venv/bin/activate
+pip install wheel setuptools
+pip install pandas decord
+pip install -r requirements.txt
+pip install flash-attn --no-build-isolation
+python setup.py develop[]
 # python wanpipeline.py
-# python preprocess_images_for_lora.py
+# python v2_wanpipeline.py
+python preprocess_images_for_lora.py
 
 if [ ! -d "$SD_SCRIPTS_DIR" ]; then
   git clone https://github.com/kohya-ss/sd-scripts.git "$SD_SCRIPTS_DIR"
@@ -39,27 +50,31 @@ pip install lycoris-lora
 pip install "numpy<2"
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True,max_split_size_mb:256
-# accelerate launch --num_processes=1 train_network.py \
-#   --pretrained_model_name_or_path stablediffusionapi/anything-v5 \
-#   --output_dir ../asset_generator/out_lora \
-#   --output_name char_lora \
-#   --save_model_as safetensors \
-#   --mixed_precision fp16 \
-#   --sdpa \
-#   --sample_every_n_steps 250 \
-#   --sample_prompts ../asset_generator/sample_prompts.json \
-#   --sample_sampler euler_a \
-#   --network_module lycoris.kohya \
-#   --network_args "algo=locon" "conv_dim=4" "conv_alpha=2" \
-#   --network_dim 16 \
-#   --network_alpha 8 \
-#   --train_data_dir ../asset_generator \
-#   --caption_extension .txt \
-#   --resolution 512 \
-#   --enable_bucket \
-#   --train_batch_size 2 \
-#   --max_data_loader_n_workers 0 \
-#   --max_train_steps 5000
+accelerate launch --num_processes=1 train_network.py \
+  --pretrained_model_name_or_path stablediffusionapi/anything-v5 \
+  --output_dir ../asset_generator/out_lora \
+  --output_name char_lora \
+  --save_model_as safetensors \
+  --mixed_precision fp16 \
+  --sdpa \
+  --sample_every_n_steps 250 \
+  --sample_prompts ../asset_generator/sample_prompts.json \
+  --network_module lycoris.kohya \
+  --network_args "algo=locon" "conv_dim=8" "conv_alpha=4" \
+  --network_dim 32 \
+  --network_alpha 16 \
+  --train_data_dir ../asset_generator \
+  --caption_extension .txt \
+  --resolution 512 \
+  --enable_bucket \
+  --train_batch_size 2 \
+  --max_data_loader_n_workers 0 \
+  --max_train_steps 5000 \
+  --sample_sampler "dpmsolver++" \
+  --sample_steps 32 \
+  --sample_width 512 --sample_height 512 \
+  --sample_cfg_scale 7.0 \
+  --sample_n_samples 3 --sample_n_rows 1
 
 deactivate
 cd "$PROJECT_DIR"
